f181e0c449771e27275cee205cb8e8b1173600a3
leena <leena.web11@gmail.com>
2018-03-09 12:39:42 +0530

    Modify package and made changes regarding fake api call

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
 import './Avatar.css';
 
-const Avatars = [
-  { "src": "images/avatar1.png", "label": "Avatar 1", "id": 1 },
-  { "src": "images/avatar2.png", "label": "Avatar 2", "id": 2 },
-  { "src": "images/avatar3.png", "label": "Avatar 3", "id": 3 },
-  { "src": "images/avatar4.png", "label": "Avatar 4", "id": 4 },
-  { "src": "images/avatar5.png", "label": "Avatar 5", "id": 5 },
-  { "src": "images/avatar6.png", "label": "Avatar 6", "id": 6 }
-]
-
 class Avatar extends Component {
   constructor() {
     super();
     this.state = {
       showPopover: false,
-      selectedAvatar: Avatars[0],
+      selectedAvatar: null,
       avatarId: null,
+      avatars: [],
     }
     this.handleClick = this.handleClick.bind(this);
     this.changeImage = this.changeImage.bind(this);
     this.handleOutsideClick = this.handleOutsideClick.bind(this);
   }
 
+  componentWillMount() {
+    fetch('http://localhost:3001/avatars?_limit=1').then(avatar => {
+      return avatar.json();
+    }).then(selectedAvatar => {
+      this.setState({ selectedAvatar: selectedAvatar[0] });
+    });     
+  }
+
   handleClick() {
     const { showPopover } = this.state;
      if (!showPopover) {
+      fetch('http://localhost:3001/avatars').then(avatars => {
+        return avatars.json();
+      }).then(avatars => {
+        this.setState({ avatars });
+      }); 
       document.addEventListener('click', this.handleOutsideClick, false);
     } else {
       document.removeEventListener('click', this.handleOutsideClick, false);
@@ -55,18 +60,18 @@ class Avatar extends Component {
   }
 
   render() {
-    const { showPopover, selectedAvatar, avatarId } = this.state;
-    console.log(avatarId);
+    const { showPopover, selectedAvatar, avatarId, avatars } = this.state;
+
     return (
       <div>
-        <img src={selectedAvatar.src} alt='Click Me!' title="Click Me!" onClick={this.handleClick} className='img' />
+        {selectedAvatar && <img src={selectedAvatar.src} alt='Click Me!' title="Click Me!" onClick={this.handleClick} className='img' />}
         {showPopover && 
         <div className='avatar-popover'>
           <p className="arrow"></p>
           <div className='avatar-list' ref={node => { this.node = node; }}>
             <label>Choose your avatar</label>
             <ul>
-              {Avatars.map(avatar => {
+              {avatars.map(avatar => {
                 return (
                   <li key={avatar.id} className={selectedAvatar.id === avatar.id ? 'active' : ''} onClick={() => this.changeImage(avatar.id)}>
                     <div className={avatarId === avatar.id ? 'spinner' : ''}></div>
